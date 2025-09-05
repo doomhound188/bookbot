10920943abf85027f97345ca957068f86123a776
doomhound188 <doomhound188@proton.me>
2025-09-04 22:56:51 -0400

    refactor main.py to improve file reading and enable CLI argument for book path

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
+import sys
 from stats import get_num_words, get_num_characters, sort_chars
 
 def get_book_text(file_path):
-    with open(file_path) as f:
+    with open(file_path, 'r', encoding='utf-8') as f:
         return f.read()
 
 def main():
-    book_path = "books/frankenstein.txt"
+    # Validate CLI usage
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    # Take book path from CLI argument
+    book_path = sys.argv[1]
+
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
 
-    text = get_book_text("books/frankenstein.txt")
+    text = get_book_text(book_path)
 
+    # Word count
     num_words = get_num_words(text)
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
 
-
+    # Character count
     char_count = get_num_characters(text)
     sorted_chars = sort_chars(char_count)
 
