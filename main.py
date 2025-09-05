841ddf9c340c1462c475fdbc6dd1e4f2bcf14807
doomhound188 <doomhound188@proton.me>
2025-09-04 22:48:28 -0400

    refactor main.py and stats.py to enhance text analysis functionality and improve output formatting

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-from stats import get_num_words
-from stats import get_num_characters
+from stats import get_num_words, get_num_characters, sort_chars
 
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
 def main():
-    book_content = get_book_text("books/frankenstein.txt")
-    num_words = get_num_words(book_content)
-    char_count = get_num_characters(book_content)
-    print(f"{num_words} words found in the document")
-    print(f"{char_count}")
+    book_path = "books/frankenstein.txt"
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+
+    text = get_book_text("books/frankenstein.txt")
+
+    num_words = get_num_words(text)
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+
+
+    char_count = get_num_characters(text)
+    sorted_chars = sort_chars(char_count)
+
+    print("--------- Character Count -------")
+    for entry in sorted_chars:
+        print(f"{entry['char']}: {entry['num']}")
+
+    print("============= END ===============")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -8,3 +8,15 @@ def get_num_characters(book_text):
     for char in text:
         chars[char] = chars.get(char, 0) + 1
     return chars
+
+def sort_chars(chars_dict):
+    char_list = []
+    for char, count in chars_dict.items():
+        if char.isalpha():
+            char_list.append({"char": char, "num": count})
+
+    def sort_on(item):
+        return item["num"]
+
+    char_list.sort(key=sort_on, reverse=True)
+    return char_list
